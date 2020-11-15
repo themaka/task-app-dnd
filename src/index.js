f7fe5f10793f784aa781addb11e9ed064855ac39
Maka Gradin <mgradin@DoCS-Maka.local>
2020-11-15 16:26:22 -0500

    basic drag-n-drop done

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+basimport { DragDropContext} from 'react-beautiful-dnd'
+import initialData from './initial-data'
+import Column from './components/Column'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = initialData
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  onDragEnd = result => {
+    const {destination, source, draggableId } = result
+
+    if(!destination) {
+      return
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+        
+
+    const column = this.state.columns[source.droppableId]
+    const newTaskIds = Array.from(column.taskIds)
+    newTaskIds.splice(source.index, 1)
+    newTaskIds.splice(destination.index, 0, draggableId)
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    }
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn,
+      }
+    }
+
+    this.setState(newState)
+
+  }
+
+  render() {
+    return (
+      <DragDropContext
+        onDragEnd={this.onDragEnd}
+      >
+        {this.state.columnOrder.map(columnId => {
+        const column = this.state.columns[columnId]
+        const tasks = column.taskIds.map(taskId => this.state.tasks[taskId])
+
+        return <Column key={column.id} column={column} tasks={tasks} /> 
+      
+        })}
+      </DragDropContext>
+    )
+  }
+}
+
+// const App = () => 'Hello world';
+
+ReactDOM.render(<App />, document.getElementById('root'))
